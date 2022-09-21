15f1791d8857123e44d487355e7bbd0dbb2df56f
MAIL479 <mail479@icloud.com>
2022-09-21 22:18:05 +0300

    ^_^

--- src/Homeworks/homework16/cat_dog/dog/Dog.java
+++ src/Homeworks/homework16/cat_dog/dog/Dog.java
@@ -8,8 +8,9 @@ public class Dog extends Animal {
     public Dog(String name) {
         this.name = name;
     }
+
     @Override
     public void sound() {
-        System.out.println("I'am a Doc. My name is "+name+". Gav");
+        System.out.println("I'am a Doc. My name is " + name + ". Gav");
     }
 }
